b5ff23c59dcd3a8edbf711fe8e8192ab83795159
wenwade <wenwei567@qq.com>
2024-09-06 20:03:29 +0800

    add hello to me

--- hello.cc
+++ hello.cc
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
 
 int sum(int a, int b) {
     return a + b;
+}
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+std::string hello_to_me(const std::string& name) {
+    return std::string{"hello "} + name;
 }
